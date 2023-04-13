bce11fec04e526892a0e381d7dc336a8d4561665
AnnaLenaW <annalena.westermann88@gmail.com>
2023-04-13 13:56:38 +0200

    Add hamburger menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,35 @@
 console.log("Welcome to my Portfolio")
+
+String
+var text1 = "Hello";
+var text2 = "Hello, spaces are also OK!";
+var text3 = 'Single quotes are valid too.';
+
+var escapedText1 = "He said: \"Yes!\", that's for sure";
+var escapedText2 = 'He said: "Yes!", that\'s for sure';
+
+Number
+var numberInteger = 456;
+var numberInteger2 = -345; // This is negative integer
+var numberFloat = 50.25;
+
+Boolean
+var boolean1 = true;
+var boolean2 = false;
+var notABoolean = "false"; //This is just the string "false", not false!
+
+var person = {
+    name: "Anna",
+    age: 35,
+    hobbies: running, skiing
+  };
+
+var age = 35;
+var name = "Anna";
+var personParent = {
+  name: name,
+  age: age,
+  child: {
+    name: "Zoe"
+  }
+};
