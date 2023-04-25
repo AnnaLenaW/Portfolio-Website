5de0e03417d7889e3c21b65c52743137ca8b0e60
AnnaLenaW <annalena.westermann88@gmail.com>
2023-04-25 22:09:32 +0200

    Revert "Changed header for L/XLdevices"
    This reverts commit af9f2187744a928eb3ae3d1973964fbc81aa44a4.

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,10 @@ console.log("Welcome to my Portfolio")
 
 function menuToggle() {
   var x = document.getElementById('myNavtoggle');
-  if ($(window).width() <= 641) { // please choose the right width
-    if (x.className === 'navtoggle') {
-      x.className += ' responsive';
-    } else {
-      x.className = 'navtoggle';
-    }
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
   }
 }
+
