af9f2187744a928eb3ae3d1973964fbc81aa44a4
AnnaLenaW <annalena.westermann88@gmail.com>
2023-04-25 22:00:56 +0200

    Changed header for L/XLdevices

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,11 @@ console.log("Welcome to my Portfolio")
 
 function menuToggle() {
   var x = document.getElementById('myNavtoggle');
-  if (x.className === 'navtoggle') {
-    x.className += ' responsive';
-  } else {
-    x.className = 'navtoggle';
+  if ($(window).width() <= 641) { // please choose the right width
+    if (x.className === 'navtoggle') {
+      x.className += ' responsive';
+    } else {
+      x.className = 'navtoggle';
+    }
   }
 }
-
